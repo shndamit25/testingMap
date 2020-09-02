5e80d285e9f2d016c29f60f9f4ca62e5d18e4610
shndamit25 <70576359+shndamit25@users.noreply.github.com>
2020-09-02 20:14:07 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Steps to run:
+    In map.js replace csvUrl values "USA.csv" and "World.csv" with respective values.
+    If CSV files are not on the same domain as your website, a CORS enabled server or a proxy is required.
+
+# csv file format:
+    value of south latitude should be negative(eg: 25.2744° S, 133.7751° E will be -25.2744,133.7751)
+    value of west longitude should be negative(eg: 37.0902° N, 95.7129° W will be 37.0902,-95.7129)
+    World.csv file can be referred for World Map
+    USA.csv file can be referred for USA Map
+
+
+# Demo url:
+    https://shndamit25.github.io/Maps/
---
+++ map.js
@@ -0,0 +1,96 @@
+require([
+  "esri/Map",
+  "esri/views/MapView",
+  "esri/layers/CSVLayer"
+], function (Map, MapView, CSVLayer) {
+
+  const popupTemplate = {
+    content: "{place}"
+  };
+
+  let iconRenderer = {
+    type: "simple",
+    symbol: {
+      type: "simple-marker",
+
+      color: [
+        0,
+        197,
+        255,
+        64
+      ],
+      size: 30,
+      xoffset: 0,
+      yoffset: 15,
+      path: "M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z"
+
+    }
+  }
+
+  let locationLabels = {
+    symbol: {
+      type: "text",
+      color: "#000000",
+      haloColor: "#FFFFFF",
+      haloSize: "1px",
+      font: {
+        size: "15px",
+        family: "Arial",
+        style: "normal",
+        weight: "bold"
+      }
+    },
+    labelPlacement: "center-center",
+    labelExpressionInfo: {
+      expression: "$feature.place"
+    }
+  };
+
+
+
+
+  document.getElementById("USAbutton").addEventListener("click", function () {
+    SetMap({
+      // If CSV files are not on the same domain as your website, a CORS enabled server
+      // or a proxy is required.
+      csvUrl: "USA.csv",
+      center: [-95.7129, 37.0902], // longitude, latitude
+      zoom: 3
+    });
+  });
+
+  document.getElementById("WorldButton").addEventListener("click", function () {
+    SetMap({
+      // If CSV files are not on the same domain as your website, a CORS enabled server
+      // or a proxy is required.
+      csvUrl: "World.csv",
+      center: [44.525455,1.677438], // longitude, latitude
+      zoom: 2
+    });
+  });
+
+  function SetMap(mapConfiguration) {
+    let map = new Map({
+      basemap: "topo-vector"
+    });
+
+    let view = new MapView({
+      container: "view",
+      map: map,
+      center: mapConfiguration.center, 
+      zoom: mapConfiguration.zoom
+    });
+
+    let csvLayer = new CSVLayer({
+      url: mapConfiguration.csvUrl,
+      labelingInfo: [locationLabels],
+      renderer: iconRenderer,
+      popupTemplate: popupTemplate
+    });
+
+    map.add(csvLayer);
+
+  }
+
+});
+
